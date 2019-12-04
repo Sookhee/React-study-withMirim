fbaa46988926f327db589ea3994435d4f4dca46e
Minji Jeong <s2018s38@e-mirim.hs.kr>
2019-12-04 23:55:57 +0900

    3.3.2 컴포넌트를 사용할 때 props 값 지정하기

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import MyComponent from './MyComponent';
 
 const App = () => {
-  return <MyComponent/>;
+  return <MyComponent name="정민지"/>;
 }
 
 export default App;
--- hello-react/src/MyComponent.js
+++ hello-react/src/MyComponent.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
-const MyComponent = ()=>{
-    return <div>나의 새롭고 멋진 컴포넌트</div>
+const MyComponent = props =>{
+    return <div>안녕하세요, 제 이름은 {props.name}입니다.</div>
+};
+
+//props 기본값 설정 : defaultProps
+MyComponent.defaultProps = {
+    name : '기본 이름'
 };
 
 export default MyComponent;
