400d3c842bc6945f08dd75786e037e7fc43bb9c3
Minji Jeong <s2018s38@e-mirim.hs.kr>
2019-12-05 01:16:08 +0900

    3.4.1.4 this.setState가 끝난 후 특정 작업 실행하기

--- hello-react/src/Counter.js
+++ hello-react/src/Counter.js
@@ -14,16 +14,15 @@ class Counter extends Component{
                 <h2>바뀌지 않는 값 : {fixedNumber}</h2>
                 <button
                 onClick={() => {
-                    this.setState(prevState => {
-                        return {
-                            number : prevState.number + 1
-                        };
-                    });
-                    //위 코드는 아래 코드와 완전히 똑같은 역할을 한다.
-                    //아래 코드는 함수에서 바로 객체를 반환한다는 의미입니다.
-                    this.setState(prevState => ({
-                        numner : prevState.number + 1
-                    }));
+                    this.setState(
+                        {
+                            number : number + 1
+                        },
+                        () => {
+                            console.log('방금 setState가 호출되었습니다.');
+                            console.log(this.state);
+                        }
+                    );
                 }}
                 >
                 +1
