c26f2bacecb07e7f86fbdd1044cd280d654d4b17
Minji Jeong <s2018s38@e-mirim.hs.kr>
2019-12-05 00:07:21 +0900

    3.3.5 비구조화 할당 문법을 통해 props 내부 값 추출하기

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import MyComponent from './MyComponent';
 
 const App = () => {
-  return <MyComponent name="정민지"/>;
+  return <MyComponent>리액트</MyComponent>;
 }
 
 export default App;
--- hello-react/src/MyComponent.js
+++ hello-react/src/MyComponent.js
@@ -1,7 +1,13 @@
 import React from 'react';
 
-const MyComponent = props =>{
-    return <div>안녕하세요, 제 이름은 {props.name}입니다.</div>
+const MyComponent = ({name, children}) =>{
+    //const {name, children} = props;
+    return (
+        <div>
+            안녕하세요, 제 이름은 {name}입니다.<br/>
+            children 값은 {children}입니다.
+        </div>
+    )
 };
 
 //props 기본값 설정 : defaultProps
