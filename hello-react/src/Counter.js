72d0bb177089d7e2c8fab20b5ac1b2e7ec486ad3
Minji Jeong <s2018s38@e-mirim.hs.kr>
2019-12-05 01:12:56 +0900

    3.4.1.3 this.setState에 객체 대신 함수 인자 전달하기

--- hello-react/src/Counter.js
+++ hello-react/src/Counter.js
@@ -1,14 +1,11 @@
 import React, {Component} from 'react';
 
 class Counter extends Component{
-    constructor(props){
-        super(props);
-        //state의 초깃값 설정하기
-        this.state = {
-            number : 0,
-            fixedNumber : 0
-        };
-    }
+    state = {
+        number : 0,
+        fixedNumber : 0
+    };
+
     render(){
         const{number, fixedNumber} = this.state; //state를 조회할 때는 this.state로 조회합니다.
         return(
@@ -17,8 +14,16 @@ class Counter extends Component{
                 <h2>바뀌지 않는 값 : {fixedNumber}</h2>
                 <button
                 onClick={() => {
-                    //this.setState를 사용하여 state에 새로운 값을 넣을 수 있습니다.
-                    this.setState({number : number + 1});
+                    this.setState(prevState => {
+                        return {
+                            number : prevState.number + 1
+                        };
+                    });
+                    //위 코드는 아래 코드와 완전히 똑같은 역할을 한다.
+                    //아래 코드는 함수에서 바로 객체를 반환한다는 의미입니다.
+                    this.setState(prevState => ({
+                        numner : prevState.number + 1
+                    }));
                 }}
                 >
                 +1
