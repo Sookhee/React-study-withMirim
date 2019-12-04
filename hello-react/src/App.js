2f2030e3ddc0e3c73b415a9173ef279fba0bdad6
Minji Jeong <s2018s38@e-mirim.hs.kr>
2019-12-05 00:47:54 +0900

    3.4.1 클래스형 컴포넌트의 state

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,11 +1,8 @@
 import React from 'react';
-import MyComponent from './MyComponent';
+import Counter from './Counter';
 
 const App = () => {
-  return <MyComponent name="정민지" favoriteNumber={7}>리액트</MyComponent>;
-
-  //name 값으로 string이 아닌 3을 넘겨줄 시 console을 확인해보면 경고 발생
-  //return <MyComponent name={3}>리액트</MyComponent>;
+  return <Counter/>;
 }
 
 export default App;
