39ebd8087360196cd03fbc25077c2f30056e334a
Minji Jeong <s2018s38@e-mirim.hs.kr>
2019-12-05 00:18:29 +0900

    3.3.6 propTypes를 통한 props 검증

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -2,7 +2,10 @@ import React from 'react';
 import MyComponent from './MyComponent';
 
 const App = () => {
-  return <MyComponent>리액트</MyComponent>;
+  return <MyComponent name="정민지" favoriteNumber={7}>리액트</MyComponent>;
+
+  //name 값으로 string이 아닌 3을 넘겨줄 시 console을 확인해보면 경고 발생
+  //return <MyComponent name={3}>리액트</MyComponent>;
 }
 
 export default App;
--- hello-react/src/MyComponent.js
+++ hello-react/src/MyComponent.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const MyComponent = ({name, children}) =>{
+const MyComponent = ({name, favoriteNumber, children}) =>{
     //const {name, children} = props;
     return (
         <div>
             안녕하세요, 제 이름은 {name}입니다.<br/>
-            children 값은 {children}입니다.
+            children 값은 {children}입니다.<br/>
+            제가 좋아하는 숫자는 {favoriteNumber}입니다.
         </div>
     )
 };
@@ -14,5 +16,12 @@ const MyComponent = ({name, children}) =>{
 MyComponent.defaultProps = {
     name : '기본 이름'
 };
+
+//name 값은 무조건 문자열(string)으로 넘겨야 한다는 의미
+MyComponent.propTypes = {
+    name : PropTypes.string,
+    favoriteNumber : PropTypes.number.isRequired
+};
+
 
 export default MyComponent;
