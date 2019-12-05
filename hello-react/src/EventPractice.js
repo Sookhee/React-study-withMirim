79a21e0cb2dd128887d8c395395367a20d415155
Minji Jeong <s2018s38@e-mirim.hs.kr>
2019-12-06 04:00:55 +0900

    4.2.3 임의 메서드 만들기

--- hello-react/src/EventPractice.js
+++ hello-react/src/EventPractice.js
@@ -6,6 +6,25 @@ class EventPractice extends Component{
         message : ''
     }
 
+    constructor(props){
+        super(props);
+        this.handleChange = this.handleChange.bind(this);
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleChange(e){
+        this.setState({
+            message: e.target.value
+        });
+    }
+
+    handleClick(){
+        alert(this.state.message);
+        this.setState({
+            message: ''
+        });
+    }
+
     render(){
         return (
             <div>
