79887795e7b97e9fb8c6420f58ed5bcc03f0ccde
Minji Jeong <s2018s38@e-mirim.hs.kr>
2019-12-18 23:54:40 +0900

    5.1 ref는 어떤 상황에서 사용해야 할까?

---
+++ hello-react/src/ValidationSample.js
@@ -0,0 +1,39 @@
+import React, {Component} from 'react';
+import './ValidationSample.css';
+
+class ValidationSample extends Component{
+    state = {
+        passward: '',
+        clicked: false,
+        validate: false
+    }
+
+    handleChange = (e) => {
+        this.setState({
+            passward: e.target.value
+        });
+    }
+
+    handleButtonClick = () => {
+        this.setState({
+            clicked: true,
+            validate: this.state.passward === '0000'
+        })
+    }
+
+    render(){
+        return (
+            <div>
+                <input
+                    type="password"
+                    value={this.state.passward}
+                    onChange={this.handleChange}
+                    className={this.state.clicked ? (this.state.validate ? 'success' : 'failure') : ''}
+                />
+                <button onClick={this.handleButtonClick}>검증하기</button>
+            </div>
+        );
+    }
+}
+
+export default ValidationSample;
