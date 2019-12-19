cc7cd59a2972b662c45ecda50e0178fd1bde51cd
Minji Jeong <s2018s38@e-mirim.hs.kr>
2019-12-20 02:27:28 +0900

    6.4.3 데이터 제거 기능 구현하기

--- hello-react/src/IterationSample.js
+++ hello-react/src/IterationSample.js
@@ -20,8 +20,17 @@ const IterationSample = () => {
         setNames(nextNames);
         setInputText('');
     }
+    const onRemove = id =>{
+        const nextNames = names.filter(name => name.id !== id);
+        setNames(nextNames);
+    }
 
-    const nameList = names.map(name => <li key={name.id}>{name.text}</li>);
+    const nameList = names.map(name => (
+        <li key={name.id} onDoubleClick={()=>onRemove(name.id)}>
+            {name.text}
+        </li>
+    ));
+    
     return (
         <>
             <input value={inputText} onChange={onChange}/>
