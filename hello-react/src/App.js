9160c0e492e0cd58240c2fa0a718f3ee51cafb8c
Minji Jeong <s2018s38@e-mirim.hs.kr>
2019-12-24 01:19:08 +0900

    8.1 useState

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,32 +1,12 @@
 import React, {Component} from 'react';
-import LifeCycleSample from './LifeCycleSample';
-import ErrorBoundary from './ErrorBoundary';
+import Counter from './Counter';
 
 function getRandomColor(){
   return '#' + Math.floor(Math.random() * 16777215).toString(16);
 }
 
-class App extends Component{
-  state = {
-    color: '#000000'
-  }
-
-  handleClick = () => {
-    this.setState({
-      color: getRandomColor()
-    });
-  }
-
-  render(){
-    return(
-      <div>
-        <button onClick={this.handleClick}>랜덤색상</button>
-        <ErrorBoundary>
-        <LifeCycleSample color={this.state.color}/>
-        </ErrorBoundary>
-      </div>
-    );
-  }
+const App = () => {
+  return <Counter/>;
 }
 
 export default App;
--- hello-react/src/Counter.js
+++ hello-react/src/Counter.js
@@ -1,35 +1,15 @@
-import React, {Component} from 'react';
+import React, {useState} from 'react';
 
-class Counter extends Component{
-    state = {
-        number : 0,
-        fixedNumber : 0
-    };
+const Counter = () =>{
+    const [value, setValue] = useState(0);
 
-    render(){
-        const{number, fixedNumber} = this.state; //state를 조회할 때는 this.state로 조회합니다.
-        return(
-            <div>
-                <h1>{number}</h1>
-                <h2>바뀌지 않는 값 : {fixedNumber}</h2>
-                <button
-                onClick={() => {
-                    this.setState(
-                        {
-                            number : number + 1
-                        },
-                        () => {
-                            console.log('방금 setState가 호출되었습니다.');
-                            console.log(this.state);
-                        }
-                    );
-                }}
-                >
-                +1
-                </button>
-            </div>
-        )
-    };
+    return (
+        <div>
+            <p>현재 카운터 값은 <b>{value}</b>입니다</p>
+            <button onClick={() => setValue(value + 1)}>+1</button>
+            <button onClick={() => setValue(value - 1)}>-1</button>
+        </div>
+    )
 }
 
 export default Counter;
