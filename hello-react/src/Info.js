5189a833d6469fb66972551ca136ba62e35663f1
Sookhee <s2018s38@e-mirim.hs.kr>
2019-12-27 04:47:35 +0900

    8.3.2 인풋 상태 관리하기

--- hello-react/src/Info.js
+++ hello-react/src/Info.js
@@ -1,30 +1,27 @@
-import React, {useState, useEffect} from 'react';
+import React, {useReducer} from 'react';
 
-const Info = () =>{
-    const [name, setName] = useState('');
-    const [nickname, setNickName] = useState('');
-    useEffect(()=>{
-        console.log('effect');
-        console.log(name);
-        return() =>{
-            console.log('cleanup');
-            console.log(name);
-        };
-    }, [name]);
-    
-    const onChangeName = e => {
-        setName(e.target.value);
-    }
+function reducer(state, action){
+    return {
+        ...state,
+        [action.name]: action.value
+    };
+}
 
-    const onChangeNickname = e => {
-        setNickName(e.target.value);
-    }
+const Info = () => {
+    const [state, dispatch] = useReducer(reducer, {
+        name: '',
+        nickname: ''
+    });
+    const {name, nickname} = state;
+    const onChange = e => {
+        dispatch(e.target);
+    };
 
     return (
         <div>
             <div>
-                <input value={name} onChange={onChangeName}/>
-                <input value={nickname} onChange={onChangeNickname}/>
+                <input name="name" value={name} onChange={onChange}/>
+                <input name="nickname" value={nickname} onChange={onChange}/>
             </div>
             <div>
                 <b>이름 : </b>{name}
