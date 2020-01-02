21401ce12e29353a933b9ae3aae2b54d9f60d968
Sookhee <s2018s38@e-mirim.hs.kr>
2020-01-02 19:31:27 +0900

    9.3 CSS Module

---
+++ styling-react/src/CSSModule.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './CSSModule.module.css';
+const CSSModule = () => {
+    return (
+        // 클래스 두개 이상 적용 시킬 때
+        <div className={`${styles.wrapper} ${styles.inverted}`}>
+            안녕하세요, 저는 <span className="something">CSS Module!</span>
+        </div>
+    );
+};
+
+export default CSSModule;
