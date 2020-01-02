a77af9158e6b97f182f0e8399a7f5671ce3d7ad7
Sookhee <s2018s38@e-mirim.hs.kr>
2020-01-02 19:42:38 +0900

    9.3.2 Sass와 함께 사용하기

--- styling-react/src/CSSModule.js
+++ styling-react/src/CSSModule.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import styles from './CSSModule.module.css';
+import styles from './CSSModule.module.scss';
 const CSSModule = () => {
     return (
         // 클래스 두개 이상 적용 시킬 때
