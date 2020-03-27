fb873adea5b6daf4d1566095ba2309cf941fc6cf
Sookhee <s2018s38@e-mirim.hs.kr>
2020-03-27 21:44:32 +0900

    12.1.5 useState의 함수형 업데이트와 immer 함께 쓰기

--- immer-tutorial/src/App.js
+++ immer-tutorial/src/App.js
@@ -14,7 +14,7 @@ const App = () => {
     e=>{
       const {name, value} = e.target;
       setForm(
-        produce(form, draft => {
+        produce(draft => {
           draft[name] = value;
         })
       );
@@ -33,7 +33,7 @@ const App = () => {
       };
 
       setData(
-        produce(data, draft => {
+        produce(draft => {
           draft.array.push(info);
         })
       );
@@ -50,7 +50,7 @@ const App = () => {
   const onRemove = useCallback(
     id=>{
       setData(
-        produce(data, draft => {
+        produce(draft => {
           draft.array.splice(draft.array.findIndex(info => info.id === id), 1);
         })
       );
