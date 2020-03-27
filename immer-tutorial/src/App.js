a827a8d23094b6e9f88aaafd4ee47049a8ca1629
Sookhee <s2018s38@e-mirim.hs.kr>
2020-03-27 21:40:15 +0900

    12.1.4 App 컴포넌트에 immer 적용하기

--- immer-tutorial/src/App.js
+++ immer-tutorial/src/App.js
@@ -1,4 +1,5 @@
 import React, {useRef, useCallback, useState} from 'react';
+import produce from 'immer';
 
 const App = () => {
   const nextId = useRef(1);
@@ -12,10 +13,11 @@ const App = () => {
   const onChange = useCallback(
     e=>{
       const {name, value} = e.target;
-      setForm({
-        ...form,
-        [name]:[value]
-      });
+      setForm(
+        produce(form, draft => {
+          draft[name] = value;
+        })
+      );
     },
     [form]
   );
@@ -30,10 +32,11 @@ const App = () => {
         username: form.username
       };
 
-      setData({
-        ...data,
-        array: data.array.concat(info)
-      });
+      setData(
+        produce(data, draft => {
+          draft.array.push(info);
+        })
+      );
 
       setForm({
         name: '',
@@ -46,10 +49,11 @@ const App = () => {
 
   const onRemove = useCallback(
     id=>{
-      setData({
-        ...data,
-        array: data.array.filter(info => info.id !== id)
-      });
+      setData(
+        produce(data, draft => {
+          draft.array.splice(draft.array.findIndex(info => info.id === id), 1);
+        })
+      );
     },
     [data]
   );
