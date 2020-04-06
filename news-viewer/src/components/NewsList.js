b15381da8d5fa1db89c503e7295bd51a6e446b55
Sookhee <s2018s38@e-mirim.hs.kr>
2020-04-06 14:25:20 +0900

    14.5 데이터 연동하기

--- news-viewer/src/components/NewsList.js
+++ news-viewer/src/components/NewsList.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import styled from 'styled-components';
 import NewsItem from './NewsItem';
+import axios from 'axios';
 
 const NewsListBlock = styled.div`
 box-sizing: border-box;
@@ -23,16 +24,42 @@ const sampleArticle = {
 };
 
 const NewsList = () => {
+    const [articles, setArticles] = useState(null);
+    const [loading, setLoading] = useState(false);
+
+    useEffect(() =>{
+        const fetchData = async () => {
+            setLoading(true);
+            try{
+                const response = await axios.get(
+                    'http://newsapi.org/v2/top-headlines?country=kr&apiKey=c6e1f1772e1b4036ab8c212686e2ec4a',
+                );
+                setArticles(response.data.articles);
+            }catch(e){
+                console.log(e);
+            }
+            setLoading(false);
+        };
+        fetchData();
+    }, []);
+
+    //대기 중일 때
+    if(loading){
+        return <NewsListBlock>대기 중...</NewsListBlock>;
+    }
+    //아직 articles 값이 설정되지 않았을 때
+    if(!articles){
+        return null;
+    }
+
+    //articles 값이 유효할 때
     return(
         <NewsListBlock>
-            <NewsItem article={sampleArticle}/>
-            <NewsItem article={sampleArticle}/>
-            <NewsItem article={sampleArticle}/>
-            <NewsItem article={sampleArticle}/>
-            <NewsItem article={sampleArticle}/>
-            <NewsItem article={sampleArticle}/>
+            {articles.map(article => (
+                <NewsItem key={articles.url} article={article}/>
+            ))}
         </NewsListBlock>
-    )
-}
+    );
+};
 
 export default NewsList;
