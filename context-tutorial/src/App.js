992b831eae93745f4bbf6b3f0164d63df7b0f179
Sookhee <s2018s38@e-mirim.hs.kr>
2020-07-17 03:45:48 +0900

    15.3.3 색상 선택 컴포넌트 만들기

--- context-tutorial/src/App.js
+++ context-tutorial/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ColorBox from './components/ColorBox';
 import {ColorProvider} from './contexts/color';
+import SelectColors from './components/SelectColors';
 
 const App = () => {
   return (
     <div>
       <ColorProvider value={{ color: 'red'}}>
         <div>
+          <SelectColors/>
           <ColorBox/>
         </div>
       </ColorProvider>
