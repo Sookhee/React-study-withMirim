3c2bc8af6d4ae0566a643f794e6f0f2fd2d3914c
Sookhee <s2018s38@e-mirim.hs.kr>
2020-07-17 03:19:21 +0900

    15.2.3 Provider

--- context-tutorial/src/App.js
+++ context-tutorial/src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import ColorBox from './components/ColorBox';
+import ColorContext from './contexts/color';
 
 const App = () => {
   return (
     <div>
-      <ColorBox/>
+      <ColorContext.Provider value={{ color: 'red'}}>
+        <div>
+          <ColorBox/>
+        </div>
+      </ColorContext.Provider>
     </div>
   );
 };
